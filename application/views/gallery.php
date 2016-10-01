1edb2046c1b798dc6b4bb6b9389a4007bbc3062f
emircado <emircado@gmail.com>
2016-09-30 20:08:38 -0700

    added sample gallery content

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -8,35 +8,35 @@
     </div>
     <div class="main wrapper clearfix">
         <div class="project-box">
-            <h2>Project name</h2>
-            <div class="project-img-main"></div>
-            <div class="project-img-strip">
-                <div class="project-img-thumb"> 
-                </div><div class="project-img-thumb">
-                </div><div class="project-img-thumb">
-                </div><div class="project-img-thumb"></div>
+            <h2>Domingo</h2>
+            <div id="prj-1" class="project-img-main"></div>
+            <div>
+                <div id="prj-2" class="project-img-thumb"> 
+                </div><div id="prj-3" class="project-img-thumb">
+                </div><div id="prj-4" class="project-img-thumb">
+                </div><div id="prj-5" class="project-img-thumb"></div>
             </div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non pretium mauris, vitae laoreet risus. 
             Sed eget fringilla eros, in molestie sem.</p>
         </div><div class="project-box">
-            <h2>Project name</h2>
-            <div class="project-img-main"></div>
-            <div class="project-img-strip">
-                <div class="project-img-thumb"> 
-                </div><div class="project-img-thumb">
-                </div><div class="project-img-thumb">
-                </div><div class="project-img-thumb"></div>
+            <h2>Virgil</h2>
+            <div id="prj-6" class="project-img-main"></div>
+            <div>
+                <div id="prj-7" class="project-img-thumb"> 
+                </div><div id="prj-8" class="project-img-thumb">
+                </div><div id="prj-9" class="project-img-thumb">
+                </div><div id="prj-10" class="project-img-thumb"></div>
             </div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non pretium mauris, vitae laoreet risus. 
             Sed eget fringilla eros, in molestie sem.</p>
         </div><div class="project-box">
-            <h2>Project name</h2>
-            <div class="project-img-main"></div>
-            <div class="project-img-strip">
-                <div class="project-img-thumb"> 
-                </div><div class="project-img-thumb">
-                </div><div class="project-img-thumb">
-                </div><div class="project-img-thumb"></div>
+            <h2>Residence</h2>
+            <div id="prj-11" class="project-img-main"></div>
+            <div>
+                <div id="prj-12" class="project-img-thumb"> 
+                </div><div id="prj-13" class="project-img-thumb">
+                </div><div id="prj-14" class="project-img-thumb">
+                </div><div id="prj-15" class="project-img-thumb"></div>
             </div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non pretium mauris, vitae laoreet risus. 
             Sed eget fringilla eros, in molestie sem.</p>
